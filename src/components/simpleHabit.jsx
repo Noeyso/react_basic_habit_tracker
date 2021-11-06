cc289697429f93c62e8335bf8832c6117bd0f5a3
noeyso <soyeon9819@gmail.com>
2021-11-06 23:16:51 +0900

    add habit tracker

---
+++ src/components/simpleHabit.jsx
@@ -0,0 +1,27 @@
+import React, { useCallback, useEffect, useRef, useState } from "react";
+
+const SimpleHabit = () => {
+  const [count, setCount] = useState(0);
+  const spanRef = useRef();
+
+  const handleIncrement = useCallback(() => {
+    setCount(count + 1);
+  });
+
+  useEffect(() => {
+    console.log(`mounted & updated!: ${count}`);
+  }, []); //두번째 인자에 어떤 값이 호출되었을때만 이 함수가 호출되도록 만들 수 있음
+  return (
+    <li className="habit">
+      <span ref={spanRef} className="habit-name">
+        Reading
+      </span>
+      <span className="habit-count">{count}</span>
+      <button className="habit-button habit-increase" onClick={handleIncrement}>
+        <i className="fas fa-plus-square"></i>
+      </button>
+    </li>
+  );
+};
+
+export default SimpleHabit;
